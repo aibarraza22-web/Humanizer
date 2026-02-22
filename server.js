8312b4ee332f87f77f09912925102a764e2e1a48
aibarraza22-web <aibarraza22@gmail.com>
2026-02-21 20:41:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { dirname, join } from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
